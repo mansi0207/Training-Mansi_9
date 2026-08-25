5959d655996f5ce065c8a01705b3560018f38a73
Mansi Patel <patelmansi02072006@gmail.com>
2026-08-25 08:36:54 +0530

    change code

---
+++ Profile_Card/script.js
@@ -0,0 +1,3 @@
+document.querySelector("img").textContent = "https://img.magnific.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg?semt=ais_hybrid&w=740&q=80"
+document.querySelector("h2").textContent = "Siddhi Patel"
+document.querySelector("p").textContent = "Java Full Stack Developer"
